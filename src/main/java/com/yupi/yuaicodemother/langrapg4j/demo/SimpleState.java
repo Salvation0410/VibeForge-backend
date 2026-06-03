3e5351bf17480ec688f63c0c3905d52897955402
yu <3248856990@qq.com>
2026-06-03 18:43:55 +0800

    feat:langraph4j 官网简单demo实现

---
+++ src/main/java/com/yupi/yuaicodemother/langrapg4j/demo/SimpleState.java
@@ -0,0 +1,31 @@
+package com.yupi.yuaicodemother.langrapg4j.demo;
+
+import org.bsc.langgraph4j.state.AgentState;
+import org.bsc.langgraph4j.state.Channels;
+import org.bsc.langgraph4j.state.Channel;
+
+import java.util.*;
+
+// 定义状态
+
+
+class SimpleState extends AgentState {
+    public static final String MESSAGES_KEY = "messages";
+
+    // Define the schema for the state.
+    // MESSAGES_KEY will hold a list of strings, and new messages will be appended.
+
+    //创建一个schema 通过appender方法增加数据 [1,] [a,2]
+    public static final Map<String, Channel<?>> SCHEMA = Map.of(
+            MESSAGES_KEY, Channels.appender(ArrayList::new)
+    );
+
+    public SimpleState(Map<String, Object> initData) {
+        super(initData);
+    }
+
+    public List<String> messages() {
+        return this.<List<String>>value("messages")
+                .orElse( List.of() );
+    }
+}
